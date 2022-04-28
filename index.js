adac1cad369fe8fd59267143f7c328f1bc3e1cdd
Taemin Kim <taeminkimny@gmail.com>
2022-04-28 15:42:49 -0400

    commit tree e0111a2b8bccacccbdef7983d90950816cb1b08e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+document.body.append(newHeader);
+
+document.getElementById('victory').textContent = '\"TAEMIN is the champion\"';
